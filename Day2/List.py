9ce55b4f8df375565b2960fd325dd51994c0e966
sameena1518 <sam01.vtechno@gmail.com>
2026-08-05 18:22:43 +0530

    Implement nested list indexing example

---
+++ Day2/List.py
@@ -0,0 +1,7 @@
+stda =[ ["name","Sam"],["age",20],[["Marks"],[23,44,99]]]
+print(stda[0][0],":",stda[0][1])
+print(stda[0][1],":",stda[1][1])
+print(stda[2][0][0],":")
+print(stda[2][1][0])
+print(stda[2][1][1])
+print(stda[2][1][2])
