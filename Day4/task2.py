53d9a84e4514e839c4a3af0469cc631c277e3429
sameena1518 <sam01.vtechno@gmail.com>
2026-08-07 11:49:09 +0530

    task 2

---
+++ Day4/task2.py
@@ -0,0 +1,12 @@
+def marks(a, b, c, d, e):
+    return a + b + c + d + e
+
+a = int(input("Enter marks 1: "))
+b = int(input("Enter marks 2: "))
+c = int(input("Enter marks 3: "))
+d = int(input("Enter marks 4: "))
+e = int(input("Enter marks 5: "))
+
+final_marks = marks(a, b, c, d, e)
+
+print("Final Marks:", final_marks)
